070b99ed32ffe3833a9deae5e139afa661f937c0
felixsteffel <felix.steffel@gmail.com>
2026-03-16 11:19:42 +0100

    Initial commit

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Settings:
-    OPCUA_ENDPOINT: str = os.getenv("OPCUA_ENDPOINT", "opc.tcp://192.168.3.10:4840")
+    OPCUA_ENDPOINT: str = os.getenv("OPCUA_ENDPOINT", "opc.tcp://192.168.3.12:4840")
     POLL_INTERVAL_SEC: float = float(os.getenv("POLL_INTERVAL_SEC", "0.5"))
     ENABLE_SIMULATOR: bool = os.getenv("ENABLE_SIMULATOR", "0") not in ("0", "false", "False")
 
@@ -12,24 +12,29 @@ class Settings:
 settings = Settings()
 
 TAGS_CONFIG = {
-    "temp_c": {
-        "type": "REAL",
-        "unit": "°C",
-        "nodeid": "ns=3;s=temp_c",
-    },
-    "pressure_bar": {
-        "type": "REAL",
-        "unit": "bar",
-        "nodeid": "ns=3;s=pressure_bar",
-    },
-    "pump_on": {
+    "always_true": {
         "type": "BOOL",
         "unit": "",
-        "nodeid": "ns=3;s=pump_on",
+        "nodeid": "ns=3;s=AlwaysTRUE",
     },
-    "valve_open": {
+    "g1_bg1_left_sensor": {
         "type": "BOOL",
         "unit": "",
-        "nodeid": "ns=3;s=valve_open",
+        "nodeid": "ns=3;s=G1-BG1",
+    },
+    "g1_bg2_right_sensor": {
+        "type": "BOOL",
+        "unit": "",
+        "nodeid": "ns=3;s=G1-BG2",
+    },
+    "c1_bg2_front_position": {
+        "type": "BOOL",
+        "unit": "",
+        "nodeid": "ns=3;s=C1-BG2",
+    },
+    "c1_bg3_rear_position": {
+        "type": "BOOL",
+        "unit": "",
+        "nodeid": "ns=3;s=C1-BG3",
     },
 }
